de4a927b893388427287418cfefca561552a9039
Oscar Adams <oadams.co@gmail.com>
2018-07-18 16:17:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function takeANumber(katzDeliLine, name){
   var i=0 
   i++
   katzDeliLine.push(`${name}`);
+  console.log(`Welcome ${name}. You are number ${i} in line.`)
   return katzDeliLine
 }
 
