966ac09a2efe5c0bbb5e60826a91908d007d41fa
Oscar Adams <oadams.co@gmail.com>
2018-07-18 17:03:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing(katzDeliLine){
  if(katzDeliLine.length === 0){
    return "There is nobody waiting to be served!"
  }
- else {
+ while(katzDeliLine.length !== 0){
    return `Currently serving ${serving}.`
    katzDeliLine.shift();
  }
