b90d6b5dd9ed6b3f09c408407b8c0e35b034d3f2
Oscar Adams <oadams.co@gmail.com>
2018-07-18 17:22:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var newArray2 = [];
  while(katzDeliLine.length !== 0){
    
    newArray2.push(`Currently serving ${serving}.`)
-   katzDeliLine.shift();
+   katzDeliLine.name.shift();
    return newArray2
  }
  
