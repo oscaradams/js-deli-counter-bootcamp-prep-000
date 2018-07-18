d800ae896461201d87d66c2c7cfbc3924339c8d0
Oscar Adams <oadams.co@gmail.com>
2018-07-18 16:13:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+
+  for(var i=1; i>0; i++){
+  katzDeliLine.push(`Welcome, ${name}. You are number ${i} in line`);
+  }
+  return katzDeliLine
+}
